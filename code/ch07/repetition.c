2776130290038211881d0d5f0f22de5a0bb448dd
hxx20 <hxxxmxx@gmail.com>
2020-12-07 14:26:08 +0900

    7장 upload

---
+++ code/ch07/repetition.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("C언어 재미있네요!\n");
+	printf("C언어 재미있네요!\n");
+	printf("C언어 재미있네요!\n");
+	
+	int n = 1;
+	//정수값을 1씩 증가시키면서 출력 반복
+	printf("%d\n", n++);
+	printf("%d\n", n++);
+	printf("%d\n", n++);
+	printf("%d\n", n++);
+	printf("%d\n", n++);
+
+	return 0;
+}
